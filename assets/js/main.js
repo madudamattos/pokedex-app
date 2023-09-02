e248186ac6a5eb3d0e0b7d65623904c4b77cbd7c
madudamattos <mariaeduardanmattos@gmail.com>
2023-09-01 21:17:20 -0300

    modificando js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,25 @@
-console.log('sucesso!');
 
+function convertPokemonToLi(pokemon){
+    return `
+        <li class="pokemon pokemon__type--grass">
+        <span class="number">#003</span>
+        <span class="name">${pokemon.name}</span>
+        
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="assets/images/bulbasaur.png">
+        </div>
+    </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+pokeApi.getPokemons().then((pokemons=[]) => {
+
+        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+
+})
